2c9be6757185934b2f02213e2ed1d015e7d95fe2
Karl Eyre <karl_eyre@hotmail.com>
2019-12-02 23:38:54 +1100

    Text Box with animation add for UI/subtitles

---
+++ Assets/Scripts/OpenTextBox.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenTextBox : MonoBehaviour
+{
+    Animator textAnimation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textAnimation = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            ChangeTextBoxStatus();
+        }
+        
+    }
+
+    public void ChangeTextBoxStatus()
+    {
+        if(textAnimation.GetBool("Open") == true)
+        {
+            textAnimation.SetBool("Open", false);
+        }
+        else
+        {
+            textAnimation.SetBool("Open", true);
+        }
+
+    }
+}
