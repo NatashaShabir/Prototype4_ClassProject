9b2fd40d2c66a0aefc10ddaffe3424abe6339cc1
aaaa <1012334@student.sae.edu.au>
2019-12-05 16:29:03 +1100

    Starting analytics again cause broke

---
+++ Assets/Scripts/AnalyticsScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class AnalyticsScript : MonoBehaviour
+{
+    public void Levels(bool startLevel1, bool endLevel1, bool startLevel2, bool endLevel2)
+    {
+        Analytics.CustomEvent("LevelProgression", new Dictionary<string, object>
+        {
+            { "Start1", startLevel1 },
+            { "End1", endLevel1 },
+            { "Start2", startLevel2 },
+            { "End2", endLevel2 }
+        });
+    }
+}
+
