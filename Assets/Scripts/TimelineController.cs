384d989ded25317ad17bce2f2924ebdb4a327cac
Natasha Shabir <1016761@gmail.com>
2019-12-06 18:59:18 +1100

    Theatre Timeline Set Up Started
    - Animations ready
    - Implementation via script in progress

---
+++ Assets/Scripts/TimelineController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineController : MonoBehaviour
+{
+    public PlayableDirector sceneDirector;
+
+    public void Play()
+    {
+        sceneDirector.Play();
+    }
+
+    public void Pause()
+    {
+        sceneDirector.Pause();
+    }
+
+    public void Resume()
+    {
+        sceneDirector.Resume();
+    }
+}
